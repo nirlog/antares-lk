c20a935a279a7d80f810863ea93dfdb9fbe3dbed
Aleksandr Lebedev <nirlog@yandex.ru>
2022-08-23 12:56:43 +0300

    верстка

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,10 @@
 import 'normalize.css/normalize.css';
+import '../vendor/select2.min.css';
+import '../scripts/select2.full.min.js';
 import './index.css';
+
+$(document).ready(function() {
+    $('.js-select2').select2({
+        theme: "classic"
+    });
+});
