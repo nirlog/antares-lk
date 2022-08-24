573c6c661255606d0a2cbecbcad30fd4ee5a34fc
Aleksandr Lebedev <nirlog@yandex.ru>
2022-08-24 15:57:22 +0300

    fix table

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import 'normalize.css/normalize.css';
+import 'jquery.scrollbar/jquery.scrollbar.css';
+import 'jquery.scrollbar/jquery.scrollbar.min.js';
 import '../vendor/select2.min.css';
-import '../scripts/select2.full.min.js';
+import '../scripts/select2.min.js';
 import './index.css';
 
 $(document).ready(function() {
+    $('.scrollbar-inner').scrollbar();
     $('.js-select2').select2({
         theme: "classic"
     });
